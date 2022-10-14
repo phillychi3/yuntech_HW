1a324922892377be86b981b6b30447d185e1373b
phillychi3 <phillychi3@gmail.com>
2022-10-14 11:35:01 +0800

    sync

--- 計算機概論/w4/ch4.py
+++ 計算機概論/w4/ch4.py
@@ -3,3 +3,9 @@ def mult(x,y=None):
         return x
     else:
         return x*y
+
+
+def multt(x,y=1):
+    return x*y
+
+print(mult(3,2))
--- 計算機概論/w4/ch5.py
+++ 計算機概論/w4/ch5.py
@@ -5,3 +5,20 @@ def log(x, base, epsilon):
     else:
         return math.log(x, base) + epsilon
 
+
+
+def logg(x, base, epsilon):
+    lower_bound = 0
+    while base**lower_bound < x:
+        lower_bound += 1
+    low = lower_bound - 1
+    high = lower_bound + 1
+    #perform bisection search
+    ans = (high + low)/base
+    while abs(base**ans - x) >= epsilon:
+        if base**ans < x:
+            low = ans
+        else:
+            high = ans
+        ans = (high + low)/base
+    print(ans, 'is close to the log base 2 of', x)
