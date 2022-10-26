4ae7838fff5cc33e09cddd79c293cba8498d0ba1
phillychi3 <phillychi3@gmail.com>
2022-10-26 13:26:03 +0800

    go

--- 計算機概論/w5/c2.py
+++ 計算機概論/w5/c2.py
@@ -1,18 +1,3 @@
-test = ['1','2','3','4','5','6','7','8','9','10']
-e = "poop"
-d = ["oppo"]
-print(test)
-test.append(e)
-print(test)
-print(test.count(e))
-test.extend(e)
-print(test)
-test.insert(0,d)
-print(test)
-print(test.index(e))
-test.pop(0)
-print(test)
-test.sort()
-print(test)
-test.reverse()
-print(test)
+L = [1,2,3]
+L.append(L)
+print( L is L[-1] )
