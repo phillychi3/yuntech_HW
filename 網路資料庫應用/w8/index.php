09f92e0b95153b5a980d6c7384ea241a6849fa3f
philly <phillychi3@gmail.com>
2022-11-10 14:35:52 +0800

    step

---
+++ 網路資料庫應用/w8/index.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <form>
+            <input type="text" name="number" />
+            <input type="text" name="name" />
+            <input type="submit" value="送出" />
+            <input type="reset" value="清除" />
+        </form>
+
+    </head>
+    <body>
+
+
+    </body>
+    <?php
+        session_start();
+
+
+
+
+    ?>
+</html>
