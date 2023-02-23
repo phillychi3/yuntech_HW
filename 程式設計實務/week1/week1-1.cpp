16767d4e262aee5df54227890a3d6db74b808fc0
phillychi3 <phillychi3@gmail.com>
2023-02-23 14:55:30 +0800

    add some

---
+++ 程式設計實務/week1/week1-1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    printf("我的名子是戚雲飛\n");
+}
---
+++ 程式設計實務/week1/week1-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    int age;
+    cout<<"請輸入姓名：";
+    cin>>name;
+    cout<<"請輸入年齡：";
+    cin>>age;
+    cout<<"你的姓名是"<<name<<"，年齡是"<<age<<"歲。"<<endl;
+}
