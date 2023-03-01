fbfc96f787c05a9f40c5fb2c406aaae0cadd6e06
phillychi3 <phillychi3@gmail.com>
2023-03-01 10:36:12 +0800

    下車

---
+++ 資料結構與算法/week1/stringslice.py
@@ -0,0 +1,12 @@
+# string slice
+
+import sys
+str = sys.stdin.readlines()
+str = [s.strip('\n') for s in str]
+
+data= str[0]
+
+flag1 = int(str[1])
+flag2 = int(str[2])
+
+print(data[flag1:flag2])
---
+++ 資料結構與算法/week1/最大連續和.py
@@ -0,0 +1,21 @@
+# 最大連續和
+
+import sys
+str = sys.stdin.readlines()
+str = [s.strip('\n') for s in str]
+
+ct = int(str[0])
+data = []
+for i in range(ct):
+    data.append(int(str[i+1]))
+
+max = 0
+now = 0
+
+for i in data:
+    now += i
+    if now > max:
+        max = now
+    if now < 0:
+        now = 0
+print(max)
