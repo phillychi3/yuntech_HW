0102e179e560e91d2c6b6980307f0625a03de514
phillychi3 <phillychi3@gmail.com>
2023-10-09 14:17:51 +0800

    sync

---
+++ 元件軟體發展技術/week3/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+// using System;
+// String bookname = "";
+// int price = 0;
+// int count = 0;
+
+// Console.WriteLine("博碩電腦圖書廣場");
+// Console.WriteLine("===============");
+// Console.Write("1. 書名:");
+// bookname = Console.ReadLine();
+// Console.Write("2. 售價:");
+// price = int.Parse(Console.ReadLine());
+// Console.Write("3. 數量:");
+// count = int.Parse(Console.ReadLine());
+// Console.WriteLine("===============");
+// Console.Write("4. 金額:"+price*count);
+
+// int price = 100;
+// int count = 30;
+// Console.WriteLine("單價:{0}    數量:{1}", price, count);
+// Console.WriteLine();
+// Console.WriteLine($"打八折後，總金額:{price*count*0.8}");
+// Console.Read();
+
+int price = 100;
+Console.WriteLine("單價:{0}    數量:{1}", price,30);
+Console.Read();
