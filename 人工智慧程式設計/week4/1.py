03627272dc87998aab96863818dabc47d2838719
phillychi3 <phillychi3@gmail.com>
2023-10-12 11:35:56 +0800

    sync

---
+++ 人工智慧程式設計/week4/1.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+v, h = np.array([1.0, 4, 9]), np.array([1.0, 3, 5, 7])
+n, m = v.sum() * h.sum() * 3, v.reshape(3, 1) * h.reshape(1, 4)
+v /= v.sum() / n
+h /= h.sum() / n
+m /= m.sum() / n
+M = np.zeros([m.shape[0] + 1, m.shape[1] + 1])
+M[:-1, -1] = v
+M[-1, :-1] = h
+M[:-1, :-1] = m
+M[-1, -1] = n
+print(
+    v.shape,
+    v,
+    v.sum(),
+    h.shape,
+    h,
+    h.sum(),
+    m.shape,
+    m,
+    m.sum(),
+    M.shape,
+    M,
+    M.sum(),
+    sep="\n",
+)
---
+++ 人工智慧程式設計/week4/tt.py
@@ -0,0 +1,25 @@
+import numpy as NP
+import matplotlib.pyplot as PL
+from functools import reduce
+
+NP.set_printoptions(linewidth=30)
+
+def mul1(a):
+    s = 1
+    for i in a:
+        s *= i
+    return s
+
+mul2 = lambda a: reduce(lambda x, y: x*y, a)
+
+sp = (2, 3, 4)
+a = NP.arange(mul1(sp))
+b = a.reshape(sp)
+print(a.shape, sep='\n')
+print(b, b.shape)
+
+sp = (2, 3, 2, 4)
+a = NP.arange(mul2(sp))
+b = a.reshape(sp)
+print(a.shape, a)
+print(b, b.shape)
