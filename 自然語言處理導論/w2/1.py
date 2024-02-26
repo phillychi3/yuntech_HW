537ddfa6594bcd733c38e548be9fa237701ad6b5
phillychi3 <phillychi3@gmail.com>
2024-02-26 11:34:34 +0800

    sync

---
+++ 自然語言處理導論/w2/1.py
@@ -0,0 +1,15 @@
+import jieba
+import os
+import jieba.posseg as psg
+
+thispath = os.path.dirname(__file__)
+jieba.load_userdict(os.path.join(thispath, "dict.txt"))
+with open(os.path.join(thispath, "file.txt"),"r",encoding="utf8") as f:
+    data = f.read()
+out = []
+
+for x in psg.cut(data):
+    if x.flag == "nr":
+        out.append(x.word)
+out = list(set(out))
+print(out)
